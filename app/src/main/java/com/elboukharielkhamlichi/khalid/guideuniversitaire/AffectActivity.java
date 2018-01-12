96b80a908fbfab103acadfc7ff74d349c89c0e4e
KhalidElKhamlichi <khalid.elboukhari.k@gmail.com>
2018-01-12 19:20:01 +0000

    Add affect activity

--- app/src/main/java/com/elboukharielkhamlichi/khalid/guideuniversitaire/AffectActivity.java
+++ app/src/main/java/com/elboukharielkhamlichi/khalid/guideuniversitaire/AffectActivity.java
@@ -2,12 +2,34 @@ package com.elboukharielkhamlichi.khalid.guideuniversitaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.elboukharielkhamlichi.khalid.guideuniversitaire.database.AppDatabase;
+import com.elboukharielkhamlichi.khalid.guideuniversitaire.entity.Etablissement;
 
 public class AffectActivity extends AppCompatActivity {
+
+    private AppDatabase appDB;
+    private RecyclerView recyclerView;
+    private EtablissementAdapter adapter;
+
+    private Etablissement e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affect);
+
+        appDB = AppDatabase.getAppDatabase(this);
+        e = (Etablissement) getIntent().getSerializableExtra("etablissement");
+
+        // set up the RecyclerView
+        recyclerView = findViewById(R.id.my_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new EtablissementAdapter(this, e.getEtablissements());
+        //adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 }
