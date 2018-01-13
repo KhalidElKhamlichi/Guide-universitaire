4c0cca5f54f4dea1c925fc9208f4befaa3a87493
KhalidElKhamlichi <khalid.elboukhari.k@gmail.com>
2018-01-13 19:05:36 +0000

    Update affect activity

--- app/src/main/java/com/elboukharielkhamlichi/khalid/guideuniversitaire/AffectActivity.java
+++ app/src/main/java/com/elboukharielkhamlichi/khalid/guideuniversitaire/AffectActivity.java
@@ -1,5 +1,8 @@
 package com.elboukharielkhamlichi.khalid.guideuniversitaire;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.elboukharielkhamlichi.khalid.guideuniversitaire.database.AppDatabase;
 import com.elboukharielkhamlichi.khalid.guideuniversitaire.entity.Etablissement;
@@ -14,14 +18,16 @@ import com.elboukharielkhamlichi.khalid.guideuniversitaire.entity.Etablissement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AffectActivity extends AppCompatActivity {
+public class AffectActivity extends AppCompatActivity implements EtablissementAdapter.ItemClickListener{
 
     private AppDatabase appDB;
     private RecyclerView recyclerView;
     private EtablissementAdapter adapter;
     private Spinner sp;
     private Etablissement e;
+    private List<Etablissement> allEtablissements;
     private List<Etablissement> etablissements;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +37,14 @@ public class AffectActivity extends AppCompatActivity {
         sp = (Spinner) findViewById(R.id.spinner);
 
         appDB = AppDatabase.getAppDatabase(this);
-        etablissements = appDB.etablissementDao().getAll();
+        allEtablissements = appDB.etablissementDao().getAll();
         List<String> spEtablissements = new ArrayList<>();
         e = (Etablissement) getIntent().getSerializableExtra("etablissement");
         /*if(e.getEtablissements() == null)
             e.setEtablissements(new ArrayList<Etablissement>());
         appDB.etablissementDao().update(e);*/
 
-        for (Etablissement et : etablissements) {
+        for (Etablissement et : allEtablissements) {
             if(et.getEid() != e.getEid()) {
                 if (!et.getType().equals("universite") && e.getType().equals("universite"))
                     spEtablissements.add(et.getNom());
@@ -55,14 +61,19 @@ public class AffectActivity extends AppCompatActivity {
         // set up the RecyclerView
         recyclerView = findViewById(R.id.affRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new EtablissementAdapter(this, getEtablissementsFromIds());
-        //adapter.setClickListener(this);
+        etablissements = getEtablissementsFromIds();
+        adapter = new EtablissementAdapter(this, etablissements);
+        adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
     }
 
     public void affecter(View view) {
+        if(!e.getType().equals("universite") && e.getEtablissements().size() > 0) {
+            Toast.makeText(this, "Supprimez l'université existante avant d'affecter une autre.", Toast.LENGTH_LONG).show();
+            return;
+        }
         String selected = sp.getSelectedItem().toString();
-        for (Etablissement et : etablissements) {
+        for (Etablissement et : allEtablissements) {
             if(et.getNom().equals(selected)) {
                 e.getEtablissements().add(et.getEid());
                 et.getEtablissements().add(e.getEid());
@@ -77,10 +88,35 @@ public class AffectActivity extends AppCompatActivity {
 
     public List<Etablissement> getEtablissementsFromIds() {
         List<Etablissement> list = new ArrayList<>();
-        for (Etablissement eta : etablissements) {
+        for (Etablissement eta : allEtablissements) {
             if(e.getEtablissements().contains(eta.getEid()))
                 list.add(eta);
         }
         return list;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
+        final Etablissement etablissement = allEtablissements.get(position);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Supprimer affectation")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        e.getEtablissements().remove(e.getEtablissements().indexOf(etablissement.getEid()));
+                        etablissement.getEtablissements().remove(etablissement.getEtablissements().indexOf(etablissement.getEid()));
+                        appDB.etablissementDao().update(e);
+                        appDB.etablissementDao().update(etablissement);
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        Dialog dialog = builder.create();
+        dialog.show();
     }
 }
